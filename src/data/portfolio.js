f4cda82f9b9eee411612def5cc90eca4f83d517f
Itay Ostraich <itayost1@gmail.com>
2025-08-20 15:49:09 +0300

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,17 +1,17 @@
 export const portfolioData = [
   {
     id: 1,
-    title: "Amos Kitchen - מערכת לניהול הזמנות אוכל וניהול מלאי",
+    title: "Kitchen Optimizer- מערכת לניהול הזמנות אוכל וניהול מלאי",
     description: "מערכת ניהול מלאי והזמנות אוכל - שמירת נתוני לקוחות כולל בעיות אלרגנים להתאמת הזמנות",
     technologies: ["React", "Node.js", "SupaBase"],
     image: "/images/AmosKitchen.png"
   },
   {
-    id: 2,
-    title: "טל נדלן - דף נחיתה לסוכן נדלן",
-    description: "דף נחיתה מותאם אישית לסוכן נדלן עם מערכת ניהול לידים",
-    technologies: ["Next.js", "Vercel"],
-    image: "/images/TalNadlan.png"
+    id: 4,
+    title: "Lola Martin - אתר תדמית למסעדה",
+    description: "אתר תדמית למסעדה עם תפריט אינטראקטיבי",
+    technologies: ["Vercel", "Next.js"],
+    image: "/images/LolaMartin.png"
   },
   {
     id: 3,
@@ -21,10 +21,10 @@ export const portfolioData = [
     image: "/images/TheFader.png"
   },
   {
-    id: 4,
-    title: "Lola Martin - אתר תדמית למסעדה",
-    description: "אתר תדמית למסעדה עם תפריט אינטראקטיבי",
-    technologies: ["Vercel", "Next.js"],
-    image: "/images/LolaMartin.png"
+    id: 2,
+    title: "טל נדלן - דף נחיתה לסוכן נדלן",
+    description: "דף נחיתה מותאם אישית לסוכן נדלן עם מערכת ניהול לידים",
+    technologies: ["Next.js", "Vercel"],
+    image: "/images/TalNadlan.png"
   }
 ];
